21c201717afdfd282af4d7042df2285779b2d2ab
Fernando <fbsilva08@hotmail.com>
2025-10-09 11:34:19 -0300

    feat: Implementa função para somar elementos de um array

--- Etapa1/src/main/java/desafio1/Main.java
+++ Etapa1/src/main/java/desafio1/Main.java
@@ -1,8 +1,36 @@
 package desafio1;
 
+import java.util.Arrays;
+
 public class Main {
 
+    public static int somarElementos(int[] numeros) {
+        if (numeros == null || numeros.length == 0) {
+            return 0;
+        }
+        int somaTotal = 0;
+        for (int numero : numeros) {
+            somaTotal += numero;
+        }
+        return somaTotal;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        System.out.println("### Testes da Função: somarElementos ###");
+
+        int[] arraySoma1 = {};
+        System.out.println("Entrada: " + Arrays.toString(arraySoma1));
+        System.out.println("Saída: " + somarElementos(arraySoma1));
+        System.out.println("--------------------");
+
+        int[] arraySoma2 = {10, 20, 30};
+        System.out.println("Entrada: " + Arrays.toString(arraySoma2));
+        System.out.println("Saída: " + somarElementos(arraySoma2));
+        System.out.println("--------------------");
+
+        int[] arraySoma3 = {-5, 10, 2, -1};
+        System.out.println("Entrada: " + Arrays.toString(arraySoma3));
+        System.out.println("Saída: " + somarElementos(arraySoma3));
     }
 }
